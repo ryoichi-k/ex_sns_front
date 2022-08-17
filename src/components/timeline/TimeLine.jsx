e896a1bec12a39fbd98d8d22f24f5d76e1696e64
ryoichi-k <kazyuapple99@gmail.com>
2022-08-17 12:59:31 +0900

    APIを叩くように実装

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Share from "../share/Share"
 import Post from "../post/Post"
 import "./TimeLine.css"
-import { Posts } from "../../dummyData.js"
+import axios from "axios"
+// import { Posts } from "../../dummyData.js"
 
 
 
 export default function TimeLine() {
+    const [posts, setPosts] = useState([]);
+
+
+    //第２引数を空にすることでマウント時に１回だけ呼ばれるようになる
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await axios.get("/posts/timeline/62f98fb2bf45a1203e8d3be6");
+            // console.log(response);
+            setPosts(response.data);
+        };
+        fetchPosts();
+    }, []);
+
   return (
       <div className="timeline">
           <div className="timelineWrapper">
               <Share />
-              { Posts.map((post) => (
+              { posts.map((post) => (
                   <Post post={post} key={ post.id } />
               ))}
           </div>
