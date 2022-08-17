382518365f7f62e2d35d571acdc5a634c2be409c
ryoichi-k <kazyuapple99@gmail.com>
2022-08-17 15:49:55 +0900

    取得api連携

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,13 +1,32 @@
 import { MoreVert } from '@mui/icons-material'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Post.css"
-import { Users } from "../../dummyData.js"
+import axios from "axios"
+
+// import { Users } from "../../dummyData.js"
 
 export default function Post({ post }) {
+    //画像パス
     const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+
+    //いいね
     const [like, setLike] = useState(post.like)
     const [isLiked, setIsLiked] = useState(false)
 
+
+    const [user, setUser] = useState({});
+
+    //useEffect第２引数を空にすることでマウント時に１回だけ呼ばれるようになる
+    useEffect(() => {
+        const fetchUser = async () => {
+            const response = await axios.get(`/users/${post.userId}`);
+            setUser(response.data);
+        };
+        console.log(user);
+        fetchUser();
+    }, []);
+
+    //いいね数の増減
     const handleLike = () => {
         setLike(isLiked ? like - 1 : like + 1)
         setIsLiked(!isLiked)
@@ -18,8 +37,8 @@ export default function Post({ post }) {
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src={ PUBLIC_FOLDER + Users.filter((user) => user.id === post.id)[0].profilePicture } alt="" className="postProfileImg" />
-                        <span className="postUserName">{ Users.filter((user) => user.id === post.id)[0].username }</span>
+                    <img src={user.profilePicture || PUBLIC_FOLDER + "/person/noAvatar.png"} alt="" className="postProfileImg" />
+                        <span className="postUserName">{ user.username }</span>
                         <span className="postDate">{post.date}</span>
                 </div>
             <div className="postTopRight">
