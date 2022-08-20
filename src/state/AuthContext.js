885c3f8237638eddd4754db8fd023c670a7762c0
ryoichi-k <kazyuapple99@gmail.com>
2022-08-20 15:09:00 +0900

    ローカルストレージにユーザー情報を保存

--- src/state/AuthContext.js
+++ src/state/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AuthReducer from "./AuthReducer";
 
 //初期値のstateを定義する。
@@ -6,18 +6,19 @@ import AuthReducer from "./AuthReducer";
 
 //初期値のユーザー状態
 const initialState = {
-    // user: null,
-    user: {
-        _id : "62f98fb2bf45a1203e8d3be6",
-        username : "tanaka",
-        email : "tanaka@gmail.com",
-        password : "tanatana",
-        profilePicture : "/person/1.jpeg",
-        coverPicture : "",
-        followers : [],
-        followings : [],
-        isAdmin : false,
-    },
+    //ローカルストレージにユーザー情報があれば取得、なければnull
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    // user: {
+    //     _id : "62f98fb2bf45a1203e8d3be6",
+    //     username : "tanaka",
+    //     email : "tanaka@gmail.com",
+    //     password : "tanatana",
+    //     profilePicture : "/person/1.jpeg",
+    //     coverPicture : "",
+    //     followers : [],
+    //     followings : [],
+    //     isAdmin : false,
+    // },
     isFetching: false,
     error: false,
 };
@@ -31,6 +32,15 @@ export const AuthContextProvider = ({ children }) => {
     //useReducerは第１引数にreducer、第二引数に初期値をとる。dispatchはアクションの通知部分。
     //stateにはユーザーが今ログインしているかどうかの状態が格納されている。
     const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+    //ローカルストレージにユーザー状態を保存
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(state.user))
+    }, [state.user]);
+
+
+
+
     //この中身をみんなで共有したい。すべたのコンポーネントでvalueのオブジェクトが使える
     return (
         <AuthContext.Provider
